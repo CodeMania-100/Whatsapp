9761f6b12ccc2aa1d04ed41879b12efd84ed7614
CodeMania-100 <achzaka100@gmail.com>
2024-08-14 20:46:17 +0300

    Removed  key from settings.py

--- whatsorganizer/settings.py
+++ whatsorganizer/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^l#u8p&%x$gzv30=0u(mk^f0z)d!3o42$wt&j_q%!dyl!&2)c('
+
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
